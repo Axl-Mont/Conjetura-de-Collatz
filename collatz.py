832fea3f9ebd36c69b212730fcf4f21731baa00f
Axl-web <axl-web.dev@protonmail.com>
2023-03-06 19:40:25 -0300

    Mejoras de la logica y el codigo

--- collatz.py
+++ collatz.py
@@ -3,10 +3,10 @@ def collatz(numerSelect):
         while numerSelect != 1:
                 if numerSelect % 2 == 0:
                         numerSelect = int(numerSelect / 2)
-                elif numerSelect % 2 != 0:
+                else:
                         numerSelect = int(numerSelect * 3 + 1)
                 cont += 1
                 print(f'Numero: {numerSelect}')
-        print(f'Cantidad de iteraciones para el ciclo: {cont}')
+        print(f'Cantidad de iteraciones para llegar al ciclo 4-2-1: {cont}')
 number = int(input('Ingresa un numero entero positivo: '))
 collatz(number)
