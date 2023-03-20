733b1ef9333451e53dcedf896311c8c04780c103
Axl-Mont <montenegroaxel@outlook.com>
2023-03-20 20:30:41 -0300

    se corrigio la divicion por // para evitar convercion innecesaria

--- collatz.py
+++ collatz.py
@@ -2,9 +2,9 @@ def collatz(numerSelect):
         cont = 0
         while numerSelect != 1:
                 if numerSelect % 2 == 0:
-                        numerSelect = int(numerSelect / 2)
+                        numerSelect = numerSelect // 2
                 else:
-                        numerSelect = int(numerSelect * 3 + 1)
+                        numerSelect = numerSelect * 3 + 1
                 cont += 1
                 print(f'Numero: {numerSelect}')
         print(f'Cantidad de iteraciones para llegar al ciclo 4-2-1: {cont}')
